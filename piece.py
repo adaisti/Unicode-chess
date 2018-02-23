78d6d67f235e933996e3710c16d999ae3732791e
adaisti <ada.hyvarinen@saunalahti.fi>
2018-02-23 14:32:50 +0200

    Korjattu muuttujan nimi

--- piece.py
+++ piece.py
@@ -10,7 +10,7 @@ class Piece():
       'rook':('♖','♜'),
       'queen':('♕','♛'),
       'king':('♔','♚'),
-      'duapple('','')
+      'apple('','')
     }
     def init_checks(self):
         diag = lambda (x1,y1), (x2,y2): abs(x1-x2) == abs(y1-y2)
